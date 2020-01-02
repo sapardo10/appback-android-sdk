8d41943ba87f7377911a07ab7c06cd53fe48a525
Sergio Pardo <carnifis@gmail.com>
2020-01-02 18:57:10 -0500

    initial commit

---
+++ appback/src/main/java/com/appback/appback/Appback.kt
@@ -0,0 +1,9 @@
+package com.appback.appback
+
+/**
+ * Entry class for the Appback library. This class will contain all the API methods
+ * necessary to use the Appback full API.
+ * @author Sergio Pardo - sapardo10
+ */
+class Appback {
+}
